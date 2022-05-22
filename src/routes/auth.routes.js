539ef41f55b0be609f398fab139d88c90609556a
Bramgelten <bramgelten@gmail.com>
2022-05-22 19:58:06 +0200

    .

--- src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router()
-const authController = require("../controllers/auth.controller")
-
-routes.post('/api/auth/login', authController.validateLogin, authController.login)
-
-module.exports = routes
-
